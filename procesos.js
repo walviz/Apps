058106290c4d05a221524dfe9f96a8028a5f3021
walviz <115000755+walviz@users.noreply.github.com>
2024-07-27 17:53:05 -0500

    Version1

--- procesos.js
+++ procesos.js
@@ -8,12 +8,14 @@ btnDeleteGuiones.addEventListener('click',()=>{
 
 
 // eliminar espacios y :
-limpiarEntrada(Legado);
-limpiarEntrada(Gis);
+limpiarEntrada(Caso);
 limpiarEntrada(CC);
-limpiarEntrada(Entra);
-limpiarEntrada(Sale);
+limpiarEntrada(IdLlamada);
+limpiarEntrada(Celular);
+limpiarEntrada(Legado);
 limpiarEntrada(Legado2);
+limpiarEntrada(switch1);
+limpiarEntrada(NE);
 function limpiarEntrada(input) {
   input.addEventListener("input", e => {
     let string = e.target.value;
@@ -41,11 +43,11 @@ function fecha(){
   var year = fecha.getFullYear();
   var mes=fecha.getMonth()+1;
   var dia=diaFecha(fecha.getDate());
-  navigator.clipboard.writeText(year+"-0"+mes+"-"+dia);
+  navigator.clipboard.writeText(dia+"/0"+mes+"/"+year);
 }
 
 // con esta funcion el texto queda selecionado
-function copyToClipBoard(parametro,idbt,idparametro) {
+function copyToClipBoard(parametro) {
   var texto = document.getElementById(parametro);
   texto.select();
   document.execCommand("copy");
@@ -65,7 +67,7 @@ function deliteTextbox(param,param2){
 
 //borra todo
 function borrarTodo(){
-  var elementos= ["Caso","IdLlamada","Legado","Gis","CC","Entra","Sale","observaciones","nota","Legado2"];
+  var elementos= ["Caso","Nombre","IdLlamada","switch1","NE","Celular","Legado","CC","Legado2","observaciones","nota",];
 
   for(let i=0;i<elementos.length;i++){
     var item=elementos[i];
@@ -81,8 +83,6 @@ function capturarTodo() {
   let legado = document.getElementById("Legado").value;
   let gis = document.getElementById("Gis").value;
   let cc = document.getElementById("CC").value;
-  let entra = document.getElementById("Entra").value;
-  let sale = document.getElementById("Sale").value;
   let observaciones = document.getElementById("observaciones").value;
   let fecha = new Date();
   //enviar toda la informacion capturada  a la plantilla
@@ -94,8 +94,6 @@ function capturarTodo() {
     "ID servicio: " + legado +"\n" +
     "ID Gis: " +gis +"\n" +
     "Cedula: " +cc +"\n" +
-    "Equipo entrante: " +entra +"\n" +
-    "Equipo saliente: " +sale +"\n" +
     "Observaciones: " + observaciones + "\n" +
     "Login: Walvizva";
   copyToClipBoard("plantilla");
@@ -143,8 +141,6 @@ function capturarTodoM6(){
   let legado = document.getElementById("Legado").value;
   let gis = document.getElementById("Gis").value;
   let cc = document.getElementById("CC").value;
-  let entra = document.getElementById("Entra").value;
-  let sale = document.getElementById("Sale").value;
   let fecha = new Date();
   document.getElementById("plantilla").value =
     "Fecha: " + converMonth(fecha.getMonth()) +"-" +diaFecha(fecha.getDate()) +"\n" +
@@ -153,8 +149,6 @@ function capturarTodoM6(){
     "ID servicio: " + legado +"\n" +
     "ID Gis: " +gis +"\n" +
     "Cedula: " +cc +"\n" +
-    "Equipo entrante: " +entra +"\n" +
-    "Equipo saliente: " +sale +"\n" +
     "Login: Walvizva";
     copyToClipBoard("plantilla");
     setTimeout(resMSS,1000);
@@ -186,117 +180,20 @@ function selecionCr(){
 //guiones select
 function selecion(){
   let opcion = document.getElementById("guiones").value;
-  let entra = document.getElementById("Entra").value;
-  let legado = document.getElementById("Legado").value;
-  let sale = document.getElementById("Sale").value;
+
   var input = document.getElementById("observaciones");
 
   switch(opcion){
     case "0":
-      document.getElementById("observaciones").value ="Buen día, se ingresa equipo " + entra + " según lo indicado, prueba integrada: ";
+      document.getElementById("observaciones").value ="La causa fue: "+"\n"+
+"La solución fue: ";
       input.focus();
       break;   
     case "1":
       document.getElementById("observaciones").value = "Buen día, No se actualiza equipo porque no nos suministra cual es el equipo que sale en reemplazo del que entra, ya que cliente cuenta en oss con la misma cantidad de equipos  que la contratada ";
       copyToClipBoard("observaciones");
       break;
-    case "2":
-      document.getElementById("observaciones").value ="Buen día compa, la MAC O serial o NSCID: " +entra +" que nos suministras NO trae información al respecto en inventario";
-      copyToClipBoard("observaciones");
-      break;
-    case "3":
-      document.getElementById("observaciones").value ="Buen día, no se corrige ya que equipo que nos indicas aparecen asignado a otro id servicio xxx  que actualmente no ha sido retirado, se debe legalizar equipos con técnico en terreno o validar correctamente los seriales.";
-        break;
-    case "4":
-      document.getElementById("observaciones").value = "Buen día, se crea CR xxx para el correcto cierre del pedido xxx y poder corregir equipos en oss";
-        break;
-    case "5":
-      document.getElementById("observaciones").value ="Buen día. en nuestra base de datos no registra el ID " +legado +" que nos indicas, se debe validar la información para la corrección del servicio.";
-      copyToClipBoard("observaciones");
-        break;
-    case "6":
-      document.getElementById("observaciones").value = "Buen día, no se corrige ya que pedido cuenta con ordenes abiertas en GTC,MSS y no es posible realizar modificaciones hasta que estas estén cerradas, Se debe hacer SS PUMED y esperar el cierre de este.";
-        break;
-    case "7":
-      document.getElementById("observaciones").value ="Buen día, Desde nuestro grupo no se actualizan equipos de ELITE por lo tanto lo debes escalar por CONSULTA LIDERES";
-      copyToClipBoard("observaciones");
-        break;
-    case "8":
-      document.getElementById("observaciones").value ="Buen día, Desde nuestro grupo no se actualizan equipos de Tecnología REDCO/GPON y servicios C3 PYMES  de ANTIOQUIA  asi sea HFC que carguen por nacional, porque el inventario y la infraestructura sigue estando montada en Fénix ATC, por lo tanto lo debes escalar por CONSULTA LIDERES.";
-      copyToClipBoard("observaciones");
-        break;
-    case "9":
-      document.getElementById("observaciones").value ="Buen día, compa no nos indicas cuan es el ID del servicio al cual hay que generar la corrección";
-      copyToClipBoard("observaciones");
-        break;
-    case "10":
-      document.getElementById("observaciones").value ="Buen día, servicio aparece con los equipos que nos indicas Prueba Integrada xxx , para crear o activar cuentas en verimatrix es con consulta lideres o rescate virtual";
-        break;
-    case "11":
-      document.getElementById("observaciones").value ="Buen día, no se puede corregir ya que servicio que nos indicas cuenta con pedido de retiro en proceso xxx  con ordenes abiertas en MSS (Baja por solicitud del cliente)";
-        break;
-    case "12":
-      document.getElementById("observaciones").value ="Buen día, se ingresa equipo " +entra +" que nos indicas al OSS, se debe validar con rescate virtual o lideres si pueden cargar paquete de canales ya que cuenta con pedidos en proceso  Prueba Integrada: ";
-      input.focus();
-        break;
-    case "13":
-      document.getElementById("observaciones").value ="Buen día, Servicio se encuentra con estado inactivo, servicio retirado";
-      copyToClipBoard("observaciones");
-        break;
-    case "14":
-      document.getElementById("observaciones").value ="Buen día, para temas de reaprovisionamiento, registro o permisos de líneas telefónicas debe ser escalado con el área de consulta lideres";
-      copyToClipBoard("observaciones");
-        break;
-    case "15":
-      document.getElementById("observaciones").value = "Buen día, compa el nscid xxxx que nos indicas nos registra con un serial totalmente diferente al que nos indicas, se debe validar correctamente la información del equipo o legalizarlo con técnico en terreno";
-      break;
-    case "16":
-      document.getElementById("observaciones").value = "Buen día, compa no se puede realizar corrección en OSS ya que actualmente servicio se encuentra suspendido por falta de pago Prueba Integrada:"
-      input.focus();
-      break;
-    case "17":
-      document.getElementById("observaciones").value = "Buen día, no se puede ingresar equipo ya que los Lite Zapper no se puede ingresar en una oferta NOV Android Trio, se debe enviar con premisas para cambio de equipo ";
-      copyToClipBoard("observaciones");
-      break;
-    case "18":
-      document.getElementById("observaciones").value = "Buen dia, se devuelve caso ya que el equipo: " +sale +" que notifican retirar no esta en oss, por favor validar cuales son los equipos que salen";
-      copyToClipBoard("observaciones");
-      break;
-    case "19":
-      document.getElementById("observaciones").value = "Buen día, no se puede asociar ya que servicio se encuentra bajo una promoción asociada diferente, se debe validar con servicio al cliente para en caso que aplique, realizar el correcto empaquetamiento del servicio. ";
-      copyToClipBoard("observaciones");
-      break;
-    case "20":
-      document.getElementById("observaciones").value ="Buen dia, compa no nos llenaste la plantilla completamente, falta el id de la llamada para realizar cualquier modificación desde nuestra área, se debe montar nuevamente y con los datos solicitados en la plantilla.";
-      copyToClipBoard("observaciones");
-      break;
-    case "21":
-      document.getElementById("observaciones").value = "Buen dia, compa no se puede asociar la telefonía al CM que nos indicas ya que este pertenece al id servicio xxx y al ser empresa, se manejan infraestructuras diferentes, si hay que asociar la to, se debe validar con premisas y verificar dicho equipo. ";
-      break;
-    case "22":
-      document.getElementById("observaciones").value ="Buen dia, equipo que nos indicas " + entra +" ya aparece en el oss del usuario, para cargar paquete de canales en los decodificadores se debe verificar con rescate virtual o lideres.Prueba Integrada: ";
-      input.focus();
-      break;
-    case "23":
-      document.getElementById("observaciones").value = "Buen dia, compa no se pueden eliminar la cantidad de equipos que nos indicas ya que usuario tiene contratado 4 equipos no se puede dejar los 3 que nos indicas, para eliminar equipo sobrante se debe validar con SAC, para poder legalizar la totalidad de equipos en oss";
-      break;
-    case "24":
-      document.getElementById("observaciones").value = "Buen dia, desde nuestra area no se realizan reconexiones de servicio, se debe validar con servicio al cliente.";
-      copyToClipBoard("observaciones");
-      break;
-    case "25":
-      document.getElementById("observaciones").value = "Buen día, servicio aparece con los equipos que nos indicas en OSS Prueba Integrada  xxx, no hay inconsistencia. se debe validar con rescate virtual o lideres si pueden cargar paquete de canales ya que cuenta con pedidos en proceso ";
-      break;
-    case "26":
-      document.getElementById("observaciones").value ="Buen día, equipo " +entra +" que nos indicas ya aparece en el oss del usuario, no hay inconsistencia, prueba integrada ";
-        break;
-    case "s":
-      document.getElementById("observaciones").value ="";
-      break;
-    case "27":
-      document.getElementById("observaciones").value ="Buen día, compa no se pueden ingresar equipo que nos indicas ya que usuario tiene contratado 3 equipos no 4 como nos indicas en el formulario, para agregar equipo  se debe validar con SAC, para poder legalizar la totalidad de equipos en OSS";
-      copyToClipBoard("observaciones");
-      break;
+
     default: false;
   }}
 
@@ -309,12 +206,16 @@ btsave.addEventListener("click", savePass);
 
 function savePass() {
   let savered = document.getElementById("passRed").value;
-  let saveWts = document.getElementById("passWts").value;
-  let saveMss = document.getElementById("passMss").value;
+  let saveEda = document.getElementById("passEda").value;
+  let saveEtp = document.getElementById("passEtp").value;
+  let saveElite = document.getElementById("passElite").value;
+
   localStorage.setItem("red", savered);
-  localStorage.setItem("wts", saveWts);
-  localStorage.setItem("mss", saveMss);
-  console.log(localStorage.getItem("red","wts","mss"));
+  localStorage.setItem("eda", saveEda);
+  localStorage.setItem("etp", saveEtp);
+  localStorage.setItem("elite", saveElite);
+
+  console.log(localStorage.getItem("red","eda","etp","elite"));
 }
 
 //cargar info en input de opciones
@@ -322,18 +223,24 @@ document.addEventListener("DOMContentLoaded", cargarValores);
 
 function cargarValores() {
   var savedRed = localStorage.getItem("red");
-  var saveWts = localStorage.getItem("wts");
-  var saveMss = localStorage.getItem("mss");
+  var saveEda = localStorage.getItem("eda");
+  var saveEtp = localStorage.getItem("etp");
+  var saveElite = localStorage.getItem("elite");
+
   if (savedRed) {
     document.getElementById("passRed").value = savedRed;
     
   }
-  if (saveWts) {
-    document.getElementById("passWts").value = saveWts;
+  if (saveEda) {
+    document.getElementById("passWts").value = saveEda;
     
   }
-  if (saveMss) {
-    document.getElementById("passMss").value = saveMss;
+  if (saveEtp) {
+    document.getElementById("passMss").value = saveEtp;
+    
+  }
+  if (saveElite) {
+    document.getElementById("passMss").value = saveElite;
     
   }
 }
@@ -342,8 +249,10 @@ function cargarValores() {
 /*funcion asignar contrasenas a los botones*/
 
 document.getElementById("btRed").addEventListener("click", () => asignarCopiar("red"));
-document.getElementById("btMss").addEventListener("click", () => asignarCopiar("mss"));
-document.getElementById("btWts").addEventListener("click", () => asignarCopiar("wts"));
+document.getElementById("btEdatel").addEventListener("click", () => asignarCopiar("eda"));
+document.getElementById("btEtp").addEventListener("click", () => asignarCopiar("etp"));
+document.getElementById("btElite").addEventListener("click", () => asignarCopiar("elite"));
+
 
 
 function asignarCopiar(valor) {
